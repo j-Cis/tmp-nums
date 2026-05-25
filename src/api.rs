6612f463d0c49308f74c5c330c5d1d9bd18f247c
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2026-05-25 16:25:10 +0200

    Update api.rs

--- src/api.rs
+++ src/api.rs
@@ -4,10 +4,27 @@
 // name = "x"
 // path = "src/api.rs"
 
-#[path = "logic/nums.rs"]
-mod nums;
-pub use nums::{NumFrac, checked_pow, nwd_gcd, nww_lcm, sequence};
+/*
+ * #[path = "logic/nums.rs"]
+ * mod nums;
+ * pub use nums::{NumFrac, checked_pow, nwd_gcd, nww_lcm, sequence};
+ *
+ * #[path = "data/formulas_data.rs"]
+ * mod formulas_data;
+ * pub use formulas_data::{Formula, Formulas, Record, Records};
+ */
 
-#[path = "data/formulas_data.rs"]
-mod formulas_data;
-pub use formulas_data::{Formula, Formulas, Record, Records};
+// 1. Klocki (Modele)
+#[path = "logic/cas.rs"]
+mod cas;
+pub use cas::{Node};
+
+// 2. Operacje pośrednie (Serwisy)
+#[path = "logic/operations.rs"]
+mod operations;
+pub use operations::{simplify_equation};
+
+// 3. Parsery (Mapowanie)
+#[path = "mapping/text_parse.rs"]
+mod text_parse;
+pub use text_parse::{parse_latex};
