1fc84ec6578fe632835d33b9d4ce8b59dcea4069
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2026-05-25 16:14:56 +0200

    Create mapping/text_parse.rs

---
+++ src/mapping/text_parse.rs
@@ -0,0 +1,7 @@
+use crate::api::Node;
+
+pub fn parse_latex(input: &str) -> Option<Node> {
+    println!("Parsowanie wejścia: {}", input);
+    // TODO: Zaimplementować rozbijanie tekstu na klocki
+    None
+}
