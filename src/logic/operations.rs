16e6328ce9c45622e85f9fd0f7fc146df9ece8ff
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2026-05-25 17:10:36 +0200

    Update operations.rs

--- src/logic/operations.rs
+++ src/logic/operations.rs
@@ -1,4 +1,5 @@
-use crate::api::Node; // Pobieramy Node przez nasze API (lub bezpośrednio z cas)
+use super::cas::Node; 
+// Pobieramy Node przez nasze API (lub bezpośrednio z cas)
 
 pub fn simplify_equation(node: &Node) -> Node {
     node.simplify()
