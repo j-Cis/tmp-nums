26b534923ab955c84e788c7913c23baa20662c11
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2026-05-25 17:19:40 +0200

    Update (fraction test) main.rs
    zabawa z ulamkami zwyklymi.

--- src/main.rs
+++ src/main.rs
@@ -46,4 +46,51 @@ fn main() {
     println!("--- Przykład 2 ---");
     println!("Przed uproszczeniem: {}", wyrazenie2.to_latex());
     println!("Po uproszczeniu:     {}", api::simplify_equation(&wyrazenie2).to_latex());
+
+
+
+println!("--- TESTY OPERACJI NA UŁAMKACH ---");
+
+    // Definiujemy zmienne (klocek a, b, c, d)
+    let a = Node::Var("a".to_string());
+    let b = Node::Var("b".to_string());
+    let c = Node::Var("c".to_string());
+    let d = Node::Var("d".to_string());
+
+    // Budujemy Ułamek 1: a/b oraz Ułamek 2: c/d
+    let ulamek1 = Node::Fr(Box::new(a.clone()), Box::new(b.clone()));
+    let ulamek2 = Node::Fr(Box::new(c.clone()), Box::new(d.clone()));
+
+    println!("Ułamek 1: {}", ulamek1.to_latex());
+    println!("Ułamek 2: {}", ulamek2.to_latex());
+    println!("----------------------------------");
+
+    // 1. MNOŻENIE: (a/b) * (c/d) -> (a * c) / (b * d)
+    let gora_mnozenie = Node::Mul(Box::new(a.clone()), Box::new(c.clone()));
+    let dol_mnozenie = Node::Mul(Box::new(b.clone()), Box::new(d.clone()));
+    let mnozenie = Node::Fr(Box::new(gora_mnozenie), Box::new(dol_mnozenie));
+    println!("Mnożenie:        {}", mnozenie.to_latex());
+
+    // 2. DZIELENIE: (a/b) / (c/d) -> (a * d) / (b * c)
+    let gora_dzielenie = Node::Mul(Box::new(a.clone()), Box::new(d.clone()));
+    let dol_dzielenie = Node::Mul(Box::new(b.clone()), Box::new(c.clone()));
+    let dzielenie = Node::Fr(Box::new(gora_dzielenie), Box::new(dol_dzielenie));
+    println!("Dzielenie:       {}", dzielenie.to_latex());
+
+    // 3. DODAWANIE: a/b + c/d -> (a*d + c*b) / (b*d)
+    let ad = Node::Mul(Box::new(a.clone()), Box::new(d.clone()));
+    let cb = Node::Mul(Box::new(c.clone()), Box::new(b.clone()));
+    let gora_dodawanie = Node::Add(Box::new(ad), Box::new(cb));
+    let dol_dodawanie = Node::Mul(Box::new(b.clone()), Box::new(d.clone()));
+    let dodawanie = Node::Fr(Box::new(gora_dodawanie), Box::new(dol_dodawanie));
+    println!("Dodawanie:       {}", dodawanie.to_latex());
+
+    // 4. ODEJMOWANIE: a/b - c/d -> (a*d - c*b) / (b*d)
+    // Realizujemy jako: a*d + (-1 * (c*b))
+    let cb_negatywne = Node::Mul(Box::new(Node::Num(-1)), Box::new(Node::Mul(Box::new(c), Box::new(b))));
+    let ad_odejmowanie = Node::Mul(Box::new(a), Box::new(d));
+    let gora_odejmowanie = Node::Add(Box::new(ad_odejmowanie), Box::new(cb_negatywne));
+    let dol_odejmowanie = Node::Mul(Box::new(b), Box::new(d));
+    let odejmowanie = Node::Fr(Box::new(gora_odejmowanie), Box::new(dol_odejmowanie));
+    println!("Odejmowanie:     {}", odejmowanie.to_latex());
 }
